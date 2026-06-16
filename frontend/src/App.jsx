9c89de4ad4f62d2b4786cd2870a933681e52224a
amandhulkar <amandhulkar0079@gmail.com>
2026-06-16 15:08:46 +0530

    fix: fix ClientDetail import path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,9 +18,7 @@ import Settings from "./pages/dashboard/Settings";
 import AccessRoles from "./pages/dashboard/AccessRoles";
 import Support from "./pages/dashboard/Support";
 
-// import ClientDetail from "./pages/dashboard/ClientDetail";
-import ClientDetail from "./pages/dashboard/Clientdetail";
-
+import ClientDetail from "./pages/dashboard/ClientDetail";
 
 
 function Layout() {
